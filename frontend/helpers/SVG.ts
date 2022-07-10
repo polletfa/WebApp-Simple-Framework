eb39ec1d9fbcdc0355dade5086038bbebc34fd07
Fabien Pollet <polletfa@posteo.de>
2022-07-10 21:47:11 +0200

    add a little doc

--- frontend/helpers/SVG.ts
+++ frontend/helpers/SVG.ts
@@ -7,11 +7,28 @@
  *
  *****************************************************/
 
+/**
+ * Define a position
+ */
 export type Pos = { x: number, y: number };
+
+/**
+ * Define the size of a rectangle (width and height)
+ */
 export type Size = { width: number, height: number };
+
+/**
+ * Define a rectangle
+ */
 export type Rect = Pos & Size;
+/**
+ * Define a font
+ */
 export type Font = { family: string, size: string, weight: string, color: string };
 
+/**
+ * Define a SVG element (can be the root or any element
+ */
 export class SVG {
     private name: string;
     private attributes: Map<string, string> = new Map();
@@ -21,11 +38,19 @@ export class SVG {
         this.name = name;
     }
 
+    /**
+     * Add an attribute
+     */
     public addAttribute(key: string, value: string): SVG {
         this.attributes.set(key, value);
         return this;
     }
 
+    /**
+     * Add a child
+     *
+     * @param child New child. Can either be another SVG element or simply a text
+     */
     public addChild(child: SVG|string): SVG {
         this.children.push(child);
         return this;
